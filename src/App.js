2e367320d46b3dc41d177a67515b44653fdbeedf
Selvakumargsk <gskumarnellai@gmail.com>
2023-08-21 10:39:01 +0530

    Updated commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<CandidateRegistrationForm/>}></Route>
+          <Route path='/NodeServerfileUploadFrontend' element={<CandidateRegistrationForm/>}></Route>
           <Route path='/candidates' element={<CandidateList/>}></Route>
         </Routes>
       </Router>
